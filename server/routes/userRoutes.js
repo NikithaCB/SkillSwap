7b83689ac7d52a635939ab15d23ebeb015ff404e
NikithaCB <nikithabhimal@gmail.com>
2025-06-05 12:33:17 +0530

    server package.json changes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const authMiddleware = require("../middleware/authMiddleware"); // Import the auth middleware
-const { Firestore } = require("firebase/firestore");
 
 // Create or update user
 // This route is now protected and updates the authenticated user's profile
